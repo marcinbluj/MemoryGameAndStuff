8a00cd745b312a3529f05333248ccd549391180a
marcinbluj <marcinbluj21@gmail.com>
2017-01-16 13:40:30 +0100

    cosmetic change

--- src/main/java/pl/memoryGame/Threading/LowestValue.java
+++ src/main/java/pl/memoryGame/Threading/LowestValue.java
@@ -37,6 +37,7 @@ public class LowestValue {
                     }
                 }
             });
+
             i = i + 1;
         }
 
--- src/main/java/pl/memoryGame/Threading/Threading3.java
+++ src/main/java/pl/memoryGame/Threading/Threading3.java
@@ -10,24 +10,52 @@ public class Threading3 {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                sum(10_000_000_000L);
+                sum(20_000_000_000L);
             }
         };
 
-        Thread thread = new Thread(runnable);
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                sum(40_000_000_000L);
+            }
+        };
+
+        Thread thread1 = new Thread(runnable);
         Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+        Thread thread4 = new Thread(runnable);
+        Thread thread5 = new Thread(runnable);
+        Thread thread6 = new Thread(runnable);
+        Thread thread7 = new Thread(runnable);
+        Thread thread8 = new Thread(runnable);
+        Thread thread9 = new Thread(runnable2);
 
         long start = System.nanoTime();
-        thread.start();
+        thread1.start();
         thread2.start();
 
-        thread.join();
+
+
+        thread1.join();
         thread2.join();
+
+
         long end = System.nanoTime();
 
 
         double duration = (end - start) / 1_000_000_000d;
         System.out.println(duration);
+
+        long start2 = System.nanoTime();
+        thread9.start();
+
+        thread9.join();
+        long end2 = System.nanoTime();
+
+
+        double duration2 = (end2 - start2) / 1_000_000_000d;
+        System.out.println(duration2);
     }
 
     public static long sum(long x) {
