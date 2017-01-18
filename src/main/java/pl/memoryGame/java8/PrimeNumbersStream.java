10d97f9e2ea47cc5a533dc9ad1d9ef0947f03779
marcinbluj <marcinbluj21@gmail.com>
2017-01-18 17:26:41 +0100

    lambda

--- src/main/java/pl/memoryGame/java8/PrimeNumbersStream.java
+++ src/main/java/pl/memoryGame/java8/PrimeNumbersStream.java
@@ -1,6 +1,7 @@
 package pl.memoryGame.java8;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,30 +9,24 @@ import java.util.stream.IntStream;
 public class PrimeNumbersStream {
     public static void main(String[] args) {
 
-        List<Integer> primeList = new ArrayList<>();
+        List<Integer> primeList = new ArrayList<>(41538);
         final boolean[] flag = new boolean[1];
 
         long start = System.nanoTime();
 
 
         primeList.add(2);
-//        primeList.add(3);
-//        primeList.add(5);
-//        primeList.add(7);
-//        primeList.add(11);
-//        primeList.add(13);
 
-        IntStream.range(3, 500_000).filter(x->
+        IntStream.range(3, 500_000).filter(x ->
                 IntStream.range(0, primeList.size())
                         .filter(y -> primeList.get(y) <= Math.sqrt(x))
-                        .noneMatch(y-> x % primeList.get(y)== 0))
+                        .noneMatch(y -> x % primeList.get(y) == 0))
                 .forEach(primeList::add);
 
 
 //        List<Integer> primaryNumbers = new ArrayList<>();
 //        List<Integer> collect = IntStream.range(2, 5_000_000).parallel().filter(x -> IntStream.range(2, (int) Math.sqrt(x) + 1)
 //                .noneMatch(y -> x % y == 0)).boxed().collect(Collectors.toList());
-
 
 
 //        IntStream.range(2, 5_000_000).filter(x -> !IntStream.range(0, primaryNumbers.size())
@@ -42,5 +37,6 @@ public class PrimeNumbersStream {
 
         System.out.println((stop - start) / 1000000000f + " s");
         System.out.println(primeList.size());
+        System.out.println(primeList.get(0));
     }
 }
