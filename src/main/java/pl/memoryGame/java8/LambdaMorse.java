7372be3f27311ae0530442790df4375f91013927
marcinbluj <marcinbluj21@gmail.com>
2017-01-18 13:50:54 +0100

    lambda map

--- src/main/java/pl/memoryGame/java8/LambdaMorse.java
+++ src/main/java/pl/memoryGame/java8/LambdaMorse.java
@@ -2,11 +2,10 @@ package pl.memoryGame.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-/**
- * Created by RENT on 2017-01-17.
- */
 public class LambdaMorse {
 
     public static String[] alpha = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
@@ -20,9 +19,42 @@ public class LambdaMorse {
             "-....", "--...", "---..", "----.", "-----", "|"};
 
 
-    public List<String> convertToMorse(String text) {
+    public static String convertToMorse(String text) {
 
+        String collect = IntStream.range(0, (text.length())).boxed().map(text::charAt)
+                .map(x -> Character.toChars(x)[0])
+                .map(x -> Arrays.stream(alpha)
+                        .map(y -> new String[]{y, Integer.toString(
+                                IntStream.range(0, alpha.length).boxed()
+                                        .filter(z -> alpha[z].equals(y)).findFirst().get())})
+                        .filter(y -> y[0].toCharArray()[0] == (x))
+                        .findFirst())
+                .map(x -> dottie[Integer.parseInt(x.get()[1])])
+                .collect(Collectors.joining(":"));
 
-        return null;
+        return collect;
+    }
+
+    public static String convertToText(String morseString) {
+
+        morseString.split(":");
+
+        String collect = IntStream.range(0, morseString.split(":").length).boxed()
+                .map(x -> new String[]{morseString.split(":")[x], Integer.toString(
+                        IntStream.range(0, dottie.length).boxed()
+                                .filter(y -> dottie[y].equals(morseString.split(":")[x])).findFirst().get())})
+                .map(x -> alpha[Integer.parseInt(x[1])])
+                .collect(Collectors.joining());
+
+        return collect;
+
+    }
+
+    public static void main(String[] args) {
+        String aabbbddcc = LambdaMorse.convertToMorse("aabbbddcc");
+        String s = LambdaMorse.convertToText(aabbbddcc);
+        System.out.println(aabbbddcc);
+        System.out.println(LambdaMorse.convertToText(aabbbddcc));
+
     }
 }
